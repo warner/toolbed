a63f1eaf3b7fcad099b342e4ea80d38ae19ba453
Brian Warner <warner@lothar.com>
2011-12-11 12:09:39 -0800

    relay input side

--- toolbed/relay.py
+++ toolbed/relay.py
@@ -1,4 +1,5 @@
 
+import collections, weakref, time
 from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import NetstringReceiver
 from twisted.application import service, strports
@@ -25,7 +26,11 @@ def split_netstrings(s):
 
 class RelayProtocol(NetstringReceiver):
     def stringReceived(self, msg):
-        pass
+        try:
+            messages = split_netstrings(msg)
+        except ValueError:
+            self.transport.loseConnection()
+        self.factory.client_message(self, messages)
 
 class RelayService(ServerFactory, service.MultiService):
     protocol = RelayProtocol
@@ -38,3 +43,26 @@ class RelayService(ServerFactory, service.MultiService):
         relayport = str(c.fetchone()[0])
         s = strports.service(relayport, self)
         s.setServiceParent(self)
+
+        # we use the WeakKeyDictionary as a WeakSet
+        self.subscriptions = collections.defaultdict(weakref.WeakKeyDictionary)
+        self.clients = weakref.WeakKeyDictionary()
+
+    def buildProtocol(self, addr):
+        p = ServerFactory.buildProtocol(self, addr)
+        self.clients[p] = {"from": addr, "connected": time.time(),
+                           "rx": 0, "tx": 0, "subscriptions": set()}
+
+    def client_message(self, p, messages):
+        self.clients[p]["rx"] += 1
+        command = messages[0]
+        if command == "subscribe":
+            address = messages[1]
+            self.subscriptions[address][p] = None
+            self.clients[p]["subscriptions"].add(address)
+        elif command == "send":
+            address = messages[1]
+            msg = messages[2]
+            for p in self.subscriptions[address].keys():
+                p.sendString(msg)
+                self.clients[p]["tx"] += 1
